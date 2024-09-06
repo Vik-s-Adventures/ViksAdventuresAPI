d9804cf41b58ea9721a5a58b792ced86c5dc88db
Shayla Choque <u20201c144@upc.edu.pe>
2024-09-06 18:24:02 -0500

    Initial commit

---
+++ src/main/java/com/upc/ViksAdventures/ViksAdventuresApplication.java
@@ -0,0 +1,13 @@
+package com.upc.ViksAdventures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViksAdventuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViksAdventuresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upc/ViksAdventures/ViksAdventuresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.ViksAdventures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViksAdventuresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
