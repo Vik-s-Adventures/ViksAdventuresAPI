f531bf4ae1d27241c038a3e97be8b11e4523a412
Shayla Choque <u20201c144@upc.edu.pe>
2024-10-18 01:36:10 -0500

    some quick fixes

--- src/main/java/com/upc/ViksAdventures/quiz/domain/persistence/ResultRepository.java
+++ src/main/java/com/upc/ViksAdventures/quiz/domain/persistence/ResultRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ResultRepository extends JpaRepository<Result, Long> {
     @Query("SELECT r FROM Result r WHERE r.student = :student AND EXISTS " +
--- src/main/java/com/upc/ViksAdventures/quiz/resource/ResultResource.java
+++ src/main/java/com/upc/ViksAdventures/quiz/resource/ResultResource.java
@@ -3,8 +3,6 @@ package com.upc.ViksAdventures.quiz.resource;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 public class ResultResource {
